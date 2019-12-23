9288ee68d3bf7ba25ea6bd9310766719d2bf3b9e
u18177657 <arkipovkirill43@gmail.com>
2019-12-23 18:38:04 +0300

    готовое приложение

--- src/components/content/content.tsx
+++ src/components/content/content.tsx
@@ -4,10 +4,10 @@ import {useEffect, useState} from "react";
 import {useParams} from 'react-router-dom';
 import './content.scss';
 
-interface Interface {
-    name: string,
-    hairColor: string,
-    eyeColor: string
+interface PersonVale{
+    name?: string,
+    hairColor?: string,
+    eyeColor?: string,
 }
 
 const Content: React.FC = () => {
@@ -15,7 +15,7 @@ const Content: React.FC = () => {
     const { id } = useParams();
     const swapi = new Swapi();
 
-    const [state, setState] = useState({
+    const [state, setState] = useState<PersonVale>({
         name:'Загрузка...',
         hairColor:'Загрузка...',
         eyeColor:'Загрузка...',
@@ -23,7 +23,7 @@ const Content: React.FC = () => {
 
     useEffect(updatePerson,[]);
 
-    function onPersonLoaded(person:Interface) {
+    function onPersonLoaded(person:PersonVale) {
         setState(person);
     }
 
@@ -34,9 +34,9 @@ const Content: React.FC = () => {
     return(
         <div className="content d-flex">
             <div className="content-info">
-                <p className="name"><span className="green">Имя персонажа: </span>{state.name}<br/></p>
-                <p className="name"><span className="green">Цвет волос: </span>{state.hairColor}</p>
-                <p className="name"><span className="green">Цвет глаз: </span>{state.eyeColor}</p>
+                <p className={state.name ? "name" : "none"}><span className="green">Имя персонажа: </span>{state.name}<br/></p>
+                <p className={state.hairColor ? "name" : "none"}><span className="green">Цвет волос: </span>{state.hairColor}</p>
+                <p className={state.eyeColor ? "name" : "none"}><span className="green">Цвет глаз: </span>{state.eyeColor}</p>
             </div>
         </div>
     )
